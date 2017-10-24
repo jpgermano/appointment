1d8346ccebd5208e5129cb0a6cadc62091c52e43
Higo Soares do Lago <higo.lago@enap.df>
2017-10-24 16:50:55 -0200

    Inclusão de arquivos no dao e mb

---
+++ src/java/dao/TbProjetoDAO.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import model.TbProjeto;
+
+/**
+ *
+ * @author hiragi
+ */
+public class TbProjetoDAO extends BaseDAO<TbProjeto> {
+    
+}
