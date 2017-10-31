4c7ae62842aed98cb2a81209c14f33d46b9bd7ff
Higo Soares do Lago <RA21485532@uniceub.edu.br>
2017-10-30 22:37:32 -0200

    CRUD Assunto quase pronto

--- src/java/dao/TbAssuntoDAO.java
+++ src/java/dao/TbAssuntoDAO.java
@@ -4,12 +4,34 @@
  */
 package dao;
 
+import java.util.List;
 import model.TbAssunto;
+import model.TbAssunto;
+import org.hibernate.Query;
 
 /**
  *
  * @author hiragi
  */
 public class TbAssuntoDAO extends BaseDAO<TbAssunto> {
-    
+        public List<TbAssunto> consultarPorReuniaoTxt(Integer idtReuniao, String txtAssunto) {
+        List<TbAssunto> lista;
+        Query qy = hib.createQuery("SELECT obj FROM TbAssunto obj "
+                + "WHERE (0=? OR tbReuniao.idtReuniao=?) AND txtAssunto LIKE ?"
+                + "ORDER BY tbReuniao.nmeLocalReuniao, txtAssunto");
+        qy.setInteger(0, idtReuniao);
+        qy.setInteger(1, idtReuniao);
+        qy.setString(2, "%" + txtAssunto + "%");
+        lista = qy.list();
+        return lista;
+    }
+
+    public List<TbAssunto> consultarPorReuniao(Integer idtReuniao) {
+        List<TbAssunto> lista;
+        Query qy = hib.createQuery("SELECT obj FROM TbReuniao obj WHERE (0=? OR tbReuniao.idtReuniao=?) ORDER BY tbReuniao.nmeLocalReuniao");
+        qy.setInteger(0, idtReuniao);
+        qy.setInteger(1, idtReuniao);
+        lista = qy.list();
+        return lista;
+    }
 }
---
+++ src/java/mb/AssuntoMB.java
@@ -0,0 +1,178 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mb;
+
+import dao.TbAssuntoDAO;
+import dao.TbReuniaoDAO;
+import model.TbAssunto;
+import model.TbReuniao;
+import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author RA21485532
+ */
+@ManagedBean
+@ViewScoped
+public class AssuntoMB {
+    /*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+    private TbAssunto selecionado;
+    private List<TbAssunto> tbAssuntos;
+    private String txtAssunto;
+    private Integer idtReuniaoPar; // Usado na combo de parâmetros de consulta
+    private List<TbReuniao> tbReuniaos;
+    private Integer idtReuniao; // Usado na combo de novo e edição
+
+    /**
+     * Creates a new instance of AssuntoMB
+     */
+    public AssuntoMB() {
+        selecionado = new TbAssunto();
+        txtAssunto = "";
+        idtReuniaoPar = 0;
+        TbReuniaoDAO dao = new TbReuniaoDAO();
+        tbReuniaos = dao.consultarTodos();
+        filtrar();
+    }
+
+    public void filtrar() {
+        TbAssuntoDAO dao = new TbAssuntoDAO();
+        setTbAssuntos(dao.consultarPorReuniaoTxt(getIdtReuniaoPar(), txtAssunto));
+    }
+
+    public void novo() {
+        setSelecionado(new TbAssunto());
+        getSelecionado().setIdtAssunto(0);
+        idtReuniao = 0;
+    }
+
+    public void salvar() {
+        TbReuniaoDAO tbReuniaoDAO = new TbReuniaoDAO();
+        selecionado.setTbReuniao(tbReuniaoDAO.consultarPorIdt(idtReuniao));
+        TbAssuntoDAO dao = new TbAssuntoDAO();
+        if (getSelecionado().getIdtAssunto() == 0) {
+            getSelecionado().setIdtAssunto(null);
+            dao.incluir(getSelecionado());
+        } else {
+            dao.juntar(getSelecionado());
+        }
+        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Resultado da Gravação", "Atualização efetivada na base de dados.");
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+        filtrar();
+    }
+
+    public void excluir() {
+        TbAssuntoDAO dao = new TbAssuntoDAO();
+        if (getSelecionado().getIdtAssunto() != 0) {
+            if (dao.excluir(getSelecionado().getIdtAssunto())) {
+                FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Resultado da Exclusão", "Exclusão efetuada com sucesso.");
+                FacesContext.getCurrentInstance().addMessage(null, msg);
+            } else {
+                FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Resultado da Exclusao", "Não foi possível excluir.");
+                FacesContext.getCurrentInstance().addMessage(null, msg);
+            }
+        }
+        filtrar();
+    }
+
+    /**
+     * @return the selecionado
+     */
+    public TbAssunto getSelecionado() {
+        return selecionado;
+    }
+
+    /**
+     * @param selecionado the selecionado to set
+     */
+    public void setSelecionado(TbAssunto selecionado) {
+        if (selecionado.getTbReuniao() != null) {
+            idtReuniao = selecionado.getTbReuniao().getIdtReuniao();
+        }
+        this.selecionado = selecionado;
+    }
+
+    /**
+     * @return the tbAssuntos
+     */
+    public List<TbAssunto> getTbAssuntos() {
+        return tbAssuntos;
+    }
+
+    /**
+     * @param tbAssuntos the tbAssuntos to set
+     */
+    public void setTbAssuntos(List<TbAssunto> tbAssuntos) {
+        this.tbAssuntos = tbAssuntos;
+    }
+
+
+    /**
+     * @return the idtReuniao
+     */
+    public Integer getIdtReuniao() {
+        return idtReuniao;
+    }
+
+    /**
+     * @param idtReuniao the idtReuniao to set
+     */
+    public void setIdtReuniao(Integer idtReuniao) {
+        this.idtReuniao = idtReuniao;
+    }
+
+    /**
+     * @return the tbReuniaos
+     */
+    public List<TbReuniao> getTbReuniaos() {
+        return tbReuniaos;
+    }
+
+    /**
+     * @param tbReuniaos the tbReuniaos to set
+     */
+    public void setTbReuniaos(List<TbReuniao> tbReuniaos) {
+        this.tbReuniaos = tbReuniaos;
+    }
+
+    /**
+     * @return the idtReuniaoPar
+     */
+    public Integer getIdtReuniaoPar() {
+        return idtReuniaoPar;
+    }
+
+    /**
+     * @param idtReuniaoPar the idtReuniaoPar to set
+     */
+    public void setIdtReuniaoPar(Integer idtReuniaoPar) {
+        this.idtReuniaoPar = idtReuniaoPar;
+    }
+
+    /**
+     * @return the txtAssunto
+     */
+    public String getTxtAssunto() {
+        return txtAssunto;
+    }
+
+    /**
+     * @param txtAssunto the txtAssunto to set
+     */
+    public void setTxtAssunto(String txtAssunto) {
+        this.txtAssunto = txtAssunto;
+    }
+}
+
+
