af48471a62612efec92a8eb7eeae7a66d6b58bcf
Higo Soares do Lago <RA21485532@uniceub.edu.br>
2017-12-08 20:28:52 -0200

    acabou

--- src/java/dao/TbCompromissoDAO.java
+++ src/java/dao/TbCompromissoDAO.java
@@ -13,14 +13,25 @@ import org.hibernate.Query;
  * @author hiragi
  */
 public class TbCompromissoDAO extends BaseDAO<TbCompromisso> {
-            public List<TbCompromisso> consultarCompromissoPorAssunto(Integer idtAssunto, String dscCompromisso) {
+    public List<TbCompromisso> consultarCompromissoPorAssunto(Integer idtAssunto, String dscCompromisso) {
         List<TbCompromisso> lista;
         Query qy = hib.createQuery("SELECT obj FROM TbCompromisso obj "
                 + "WHERE (0=? OR tbAssunto.idtAssunto=?) AND dscCompromisso LIKE ?"
-                + "ORDER BY tbAssunto.txtAssunto, dscCompromisso");
+                + "ORDER BY tbAssunto.idtAssunto");
         qy.setInteger(0, idtAssunto);
         qy.setInteger(1, idtAssunto);
         qy.setString(2, "%" + dscCompromisso + "%");
+        lista = qy.list();
+        return lista;
+    }
+            
+    public List<TbCompromisso> consultarPorAssunto(Integer idtAssunto) {
+        List<TbCompromisso> lista;
+        Query qy = hib.createQuery("SELECT obj FROM TbCompromisso obj "
+                + "WHERE (0=? OR tbAssunto.idtAssunto=?)"
+                + "ORDER BY tbAssunto.idtAssunto");
+        qy.setInteger(0, idtAssunto);
+        qy.setInteger(1, idtAssunto);
         lista = qy.list();
         return lista;
     }
